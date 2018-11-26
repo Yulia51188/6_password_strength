a09addb50c3a5f73a453c93d0056bd1a5ad6375b
Yulia51188 <sun51188@gmail.com>
2018-11-26 19:54:54 +0300

    Доработки pep-8. Проверено на web- сервисе

--- password_strength.py
+++ password_strength.py
@@ -8,7 +8,7 @@ DEFAULT_BLACK_LIST = ('123456', '123456789', '111111',
                       'password1', '1234567', '123123',)
 
 
-def validate_date (input_string):
+def validate_date(input_string):
     try:
         datetime.strptime(input_string, "%d.%m.%y")
         return input_string.split('.')
@@ -17,7 +17,7 @@ def validate_date (input_string):
         raise argparse.ArgumentTypeError(msg)
 
 
-def parse_arguments ():
+def parse_arguments():
     parser = argparse.ArgumentParser(
         description='Test your password reliability'
     )
@@ -28,7 +28,7 @@ def parse_arguments ():
     parser.add_argument('-ln', '--last_name', type=str,
                         help='user last name', default=None)
     parser.add_argument('-bd', '--birthday', type=validate_date,
-                        help='user birthday in format dd.mm.yy', default=[ ])
+                        help='user birthday in format dd.mm.yy', default=[])
     parser.add_argument('-c', '--company', type=str, default=None,
                         help='company name or abbreviation')
     parser.add_argument('-e', '--email', type=str, help='user email',
@@ -38,13 +38,13 @@ def parse_arguments ():
     return parser.parse_args()
 
 
-def join_personal_info (list_of_data):
-    personal_info = [element for element in list_of_data if not element is None]
+def join_personal_info(data_list):
+    personal_info = [element for element in data_list if element is not None]
     if len(personal_info):
         return personal_info
 
 
-def count_number_of_symbols_types (password):
+def count_number_of_symbols_types(password):
     number_of_types = sum([
         bool(re.search('\d', password)),
         bool(re.search('[a-z]', password)),
@@ -56,8 +56,7 @@ def count_number_of_symbols_types (password):
     return number_of_types
 
 
-def check_entry_in_extended_black_list (password,
-                                        filename_black_list):  # выбор списка и расчет баллов перенести в общую оценку
+def check_entry_in_extended_black_list(password, filename_black_list):
     with open(filename_black_list, 'r') as file_object:
         str_data = file_object.read()
     black_list = str_data.split('\n')
@@ -66,14 +65,14 @@ def check_entry_in_extended_black_list (password,
     return False
 
 
-def check_entry_of_personal_info (password, personal_info):
+def check_entry_of_personal_info(password, personal_info):
     entry = False
     for word in personal_info:
         entry = bool(re.search(word.lower(), password.lower()))
     return entry
 
 
-def get_password_strength (password, personal_info, filename_black_list):
+def get_password_strength(password, personal_info, filename_black_list):
     password_strength = sum([
         1,
         int(len(password) > 8),
@@ -83,11 +82,11 @@ def get_password_strength (password, personal_info, filename_black_list):
     if password in DEFAULT_BLACK_LIST:
         return 0
     password_strength += 1
-    if not filename_black_list is None and \
+    if filename_black_list is not None and \
             check_entry_in_extended_black_list(password, filename_black_list):
         return 0
     password_strength += 1
-    if not personal_info is None:
+    if personal_info is not None:
         if check_entry_of_personal_info(password, personal_info):
             password_strength += -3
         else:
@@ -99,7 +98,7 @@ def get_password_strength (password, personal_info, filename_black_list):
 
 if __name__ == '__main__':
     args = parse_arguments()
-    if not args.black_list is None and not os.path.exists(args.black_list):
+    if args.black_list is not None and not os.path.exists(args.black_list):
         print("File with black list doesn't exist")
     personal_info = join_personal_info(
         [args.first_name,
